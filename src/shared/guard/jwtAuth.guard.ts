6f3bf27f9c1ccb56f67158ac74c16f1bd6d0b6f2
analuciahm27 <125940874+analuciahm27@users.noreply.github.com>
2026-02-15 22:38:04 -0500

    feat: implementa autenticación con HttpOnly cookies (#15)

--- src/shared/guard/jwtAuth.guard.ts
+++ src/shared/guard/jwtAuth.guard.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { TokenExpiredError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class jwtAuthGuard extends AuthGuard('jwt') {
+    handleRequest<TUser = unknown>(err: unknown, user: TUser, info: unknown): TUser {
+
+        if (info instanceof TokenExpiredError) {
+        throw new UnauthorizedException('El token ha expirado. Inicia sesión nuevamente.');
+        }
+
+        if (err || !user) {
+            throw err || new Error('No se ha podido autenticar al usuario');
+        }
+        return user;
+    }
 }
