780ac04e4354f2818624ed40152caca7b8b5f247
David <dforero42716@gmail.com>
2026-08-13 17:14:47 -0500

    feat: implementa endpoints de gestión de cursos asignados (#73)

---
+++ archivo.sql
@@ -0,0 +1,3 @@
+INSERT INTO auth.cuenta_asociada (idpadre, tipocuenta, estado, alias, fechacreacion) VALUES (7, 'JOVEN', 'ACTIVA', 'Prueba 2', now());
+
+
---
+++ insert-prueba.sql
@@ -0,0 +1,6 @@
+INSERT INTO auth.cuenta_asociada (idpadre, tipocuenta, estado, alias, fechacreacion) VALUES (7, 'NINO', 'ACTIVA', 'Hijo de prueba', now());
+INSERT INTO auth.cuenta_asociada (idpadre, tipocuenta, estado, alias, fechacreacion) VALUES (7, 'JOVEN', 'ACTIVA', 'Prueba 2', now());
+
+
+
+
